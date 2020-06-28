31ec7eddefca3f343e901604937d9648ad56e6b2
usong2 <dbthd6@naver.com>
2020-06-28 23:37:45 +0900

    useRef로 특정 DOM 선택

---
+++ useRef.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+
+function InputForm() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+    age: "",
+  });
+  const nameInput = useRef();
+  const { name, nickname, age } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onReset = (e) => {
+    setInputs({
+      name: "",
+      nickname: "",
+      age: "",
+    });
+    nameInput.current.focus();
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        type="text"
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <input
+        type="text"
+        name="age"
+        placeholder="나이"
+        onChange={onChange}
+        value={age}
+      />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값: </b>
+        {name}({nickname}){age}
+      </div>
+    </div>
+  );
+}
+
+export default InputForm;
